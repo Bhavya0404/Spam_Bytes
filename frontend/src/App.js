9c1290e1dfb72687a7afc0aeb56d8ab01fcb865f
Abhiram <abhirambsn@gmail.com>
2022-06-21 19:32:03 +0530

    Frontend Reset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Form from "./Components/Form";
-import Axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  Axios({
-    method: "GET",
-    url: "http://localhost:5000/",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
-    console.log(res.data.message);
-  });
-
   return (
     <div className="App">
-      <Form />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
