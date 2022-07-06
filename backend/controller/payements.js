77a019c66e9be53ed681837279004477707e8f70
Abhiram B.S.N <Abhiram B.S.N>
2022-07-06 16:24:02 +0530

    Payments Implementation Started

---
+++ backend/controller/payements.js
@@ -0,0 +1,9 @@
+const Razorpay = require('razorpay')
+var instance = new Razorpay({
+    key_id: process.env.RAZORPAY_TEST_KEYID,
+    key_secret: process.env.RAZORPAY_TEST_SECRET
+})
+
+const addDetails = () => {
+
+}
