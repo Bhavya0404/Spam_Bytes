e9b5c3597b1539523e6a7c45214c06246b47c363
Bhavya0404 <singhpappu966@gmail.com>
2022-07-12 22:23:02 +0530

    deleted json files

--- backend/controller/ngo.js
+++ backend/controller/ngo.js
@@ -1,10 +1,10 @@
-// const ngoData = require('../model/ngo');
-const ngoData = require('./ngoData.json');
+const ngoData = require('../model/ngo');
+
 const getNgo = async (req, res) => {
     try {
-        // const ngoInfo = await ngoData.find();
+        const ngoInfo = await ngoData.find();
 
-        res.status(200).json(ngoData);
+        res.status(200).json(ngoInfo);
     } catch (error) {
         res.status(400).json({message : error.message});
     }
