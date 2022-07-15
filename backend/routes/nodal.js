b8d0693a87dfa5bc86f49a04ee5199f2c20dbd0a
Abhiram B.S.N <abhirambsn@gmail.com>
2022-07-15 11:47:54 +0530

    Payment Integration Started

--- backend/routes/nodal.js
+++ backend/routes/nodal.js
@@ -1,10 +1,12 @@
 const { getNodal, verifyChild } = require('../controller/nodal');
 const express = require('express');
 const { isAuthenticated } = require('../middleware/auth');
+const { createContact } = require('../controller/payements');
 
 const router = express.Router();
 
 router.get('/', getNodal);
 router.put('/verify/:id', isAuthenticated, verifyChild)
+router.post('/createContact', createContact)
 
 module.exports = router;
