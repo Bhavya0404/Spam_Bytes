ce27cb3e826e8be5ca29b5757497d3a10d352742
Abhiram B.S.N <abhirambsn@gmail.com>
2022-07-20 10:50:02 +0530

    Backend Bug Fix

--- backend/controller/nodal.js
+++ backend/controller/nodal.js
@@ -15,7 +15,7 @@ const getNodal = async (req, res) => {
     const nodal = await nodalData.find();
     res.status(200).json(nodal);
   } catch (error) {
-    console.log(nodal);
+    console.log(error);
     res.status(400).json({ message: error.message });
   }
 };
