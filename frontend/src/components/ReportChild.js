48c9ce40e714252f31e921f5912e9a0de96e55ed
udittripathi <71835574+udittripathi@users.noreply.github.com>
2022-08-19 15:49:06 +0530

    reportchild

--- frontend/src/components/ReportChild.js
+++ frontend/src/components/ReportChild.js
@@ -1,6 +1,14 @@
+// import { TextField } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+// import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from "@mui/material";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import { Typography } from "@mui/material";
 import {
   getFoundChildStatus,
   selectFoundChild,
@@ -57,7 +65,7 @@ const ReportChild = () => {
       );
       if (resp.status === 201) {
         console.log(resp);
-        alert(resp?.statusText);
+        // alert(resp?.statusText);
       } else {
         alert("Error");
         console.log(resp);
@@ -89,65 +97,170 @@ const ReportChild = () => {
       setDistrict("");
     }
   };
-  return (
-    <div>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="desc">Description</label>
-        <textarea id="desc" onChange={(e) => setDescription(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="img">Image</label>
-        <input
-          id="img"
-          type="file"
-          onChange={(e) => {
-            const file = e.target.files[0];
-            if (file) {
-              const reader = new FileReader();
-              reader.onload = (rEvent) => {
-                const bString = rEvent.target.result;
-                setImg(btoa(bString));
-              };
-              reader.readAsBinaryString(file);
-            }
-            console.log(img);
+  // return (
+  //   <div>
+  //     <div>
+  //       <label htmlFor="name">Name</label>
+  //       <input
+  //         id="name"
+  //         type="text"
+  //         value={name}
+  //         onChange={(e) => setName(e.target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       <label htmlFor="desc">Description</label>
+  //       <textarea id="desc" onChange={(e) => setDescription(e.target.value)} />
+  //     </div>
+  //     <div>
+  //       <label htmlFor="img">Image</label>
+  //       <input
+  //         id="img"
+  //         type="file"
+  //         onChange={(e) => {
+  //           const file = e.target.files[0];
+  //           if (file) {
+  //             const reader = new FileReader();
+  //             reader.onload = (rEvent) => {
+  //               const bString = rEvent.target.result;
+  //               setImg(btoa(bString));
+  //             };
+  //             reader.readAsBinaryString(file);
+  //           }
+  //           console.log(img);
+  //         }}
+  //       />
+  //     </div>
+  //     <div>
+  //       <label htmlFor="address">Address</label>
+  //       <textarea id="address" onChange={(e) => setAddress(e.target.value)} />
+  //     </div>
+  //     <div>
+  //       <label htmlFor="state">State</label>
+  //       <input
+  //         id="state"
+  //         type="text"
+  //         onChange={(e) => setState(e.target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       <label htmlFor="dist">District</label>
+  //       <input
+  //         id="dist"
+  //         type="text"
+  //         onChange={(e) => setDistrict(e.target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       <button onClick={handleReportChild}>Report Child</button>
+  //     </div>
+  //   </div>
+  // );
+  
+  // export default function FormPropsTextFields() {
+    
+    return (
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          height: "100vh",
+          alignItems: "center",
+          justifyContent: "center",
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+         <Paper
+        elevation={12}
+        sx={{
+          width: { xs: "320px", sm: "500px", md: "550px" },
+          height: { xs: "650px", sm: "650px", md: "700px", lg: "750px" },
+          backgroundColor: "#FFFFFF",
+          display: "flex",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Container
+          sx={{
+            width: { xs: "80%", md: "70%" },
+            height: "75%",
           }}
-        />
-      </div>
-      <div>
-        <label htmlFor="address">Address</label>
-        <textarea id="address" onChange={(e) => setAddress(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="state">State</label>
-        <input
-          id="state"
-          type="text"
-          onChange={(e) => setState(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="dist">District</label>
-        <input
-          id="dist"
-          type="text"
-          onChange={(e) => setDistrict(e.target.value)}
-        />
-      </div>
-      <div>
-        <button onClick={handleReportChild}>Report Child</button>
-      </div>
-    </div>
-  );
+          >
+              <Typography
+            variant="h4"
+            component="p"
+            sx={{
+              fontWeight: 600,
+              mb: 5,
+              fontSize: { xs: 25, sm: 32 },
+            }}
+          >
+            Report Child
+          </Typography>
+
+        <TextField id="outlined-basic" fullWidth required label="Name" variant="outlined" value={name} onChange={(e) => setName(e.target.value)}/>
+          <TextField
+            required
+            fullWidth
+            sx={{ mt: "20px" }}
+            id="outlined-required"
+            label="Description" 
+            value={description} 
+            onChange={(e) => setDescription(e.target.value)}         
+          />
+          <Button variant="contained" sx={{ mt: "2px", width: "50%"}} component="label" value={img}
+           onChange={(e) => {
+                      const file = e.target.files[0];
+                      if (file) {
+                        const reader = new FileReader();
+                        reader.onload = (rEvent) => {
+                          const bString = rEvent.target.result;
+                          setImg(btoa(bString));
+                        };
+                        reader.readAsBinaryString(file);
+                      }
+                      console.log(img);
+                    }}>
+  Upload
+  <input hidden accept="image/*" multiple type="file" />
+</Button>
+          <TextField
+          required
+          fullWidth
+            id="outlined-required"
+            label="Address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <TextField
+          required
+          fullWidth
+            id="outlined-required"
+            label="District"
+            value={district}
+            onChange={(e) => setDistrict(e.target.value)}
+          />
+          <TextField
+          required
+          fullWidth
+            id="outlined-required"
+            label="State"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+          />
+          <Button onClick={handleReportChild} 
+          size="large"
+            sx={{ mt: "30px", backgroundColor: "black", width: "70%" }} 
+            variant="contained">
+              Submit
+              </Button>
+        </Container>
+      </Paper>
+    </Box>
+    );
 };
 
 export default ReportChild;
