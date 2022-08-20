f6d19de3e42a5eab9fe52c25f2000daf0d11f56e
Meenal Prakash <meenalprakash03@gmail.com>
2022-08-20 23:15:22 +0530

    goverment

---
+++ frontend/src/components/Goverment.js
@@ -0,0 +1,54 @@
+import React from "react";
+import AnimatedRoutes from "./AnimatedRoutes";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/material";
+
+const Government = () => {
+  return (
+    <div>
+      <h1>National Child Labour Project SCHEME</h1>
+      <h2>Objective of the Scheme:</h2>
+      <p>
+        This is the major Central Sector Scheme for the rehabilitation of child
+        labour. The Scheme seeks to adopt a sequential approach with focus on
+        rehabilitation of children working in hazardous occupations & processes
+        in the first instance. Under the Scheme, survey of child labour engaged
+        in hazardous occupations & processes has been conducted. The identified
+        children are to be withdrawn from these occupations & processes and then
+        put into special schools in order to enable them to be mainstreamed into
+        formal schooling system. Project Societies at the district level are
+        fully funded for opening up of special schools/Rehabilitation Centres
+        for the rehabilitation of child labour. The special
+        schools/Rehabilitation Centres provide: Non-formal/bridge education
+        Skilled/vocational training Mid Day Meal Stipend @ Rs.150/- per child
+        per month. Health care facilities through a doctor appointed for a group
+        of 20 schools.
+      </p>
+      <p>
+        The Target group: The project societies are required to conduct survey
+        to identify children working in hazardous occupations and processes.
+        These children will then form the target group for the project society.
+        Of the children identified those in the age group 5-8 years will have to
+        be mainstreamed directly to formal educational system through the SSA.
+        Working children in the age group of 9- 14 years will have to be
+        rehabilitated through NCLP schools established by the Project Society.
+        Project Implementation: The entire project is required to be implemented
+        through a registered society under the Chairmanship of administrative
+        head of the district, namely, District Magistrate/Collector/Dy.
+        Commissioner of the District. Members of the society may be drawn from
+        concerned Government Departments, representatives of Panchayati Raj
+        Institutions, NGOs, Trade Unions, etc. Funding pattern: The projects
+        have been taken up in the Central Sector, the entire funding is done by
+        the Central Government (Ministry of Labour & Employment). Funds are
+        released to the concerned Project Societies depending upon the progress
+        of project activities. Present Status of NCLP Scheme: At present about
+        6000 special schools are in operation under NCLP scheme. As on date more
+        than 10 lakhs children have been mainstreamed into the formal education
+        system under the Scheme.
+      </p>
+    </div>
+  );
+};
+
+export default Government;
