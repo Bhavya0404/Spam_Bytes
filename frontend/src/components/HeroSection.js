98a14fbb35bbefa68672f93c887fc6535d68589d
Ishita-Yadav-35 <84377800+Ishita-Yadav-35@users.noreply.github.com>
2022-08-21 15:03:49 +0530

    homepage

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -7,6 +7,11 @@ import DonateButton from './DonateButton'
 import { useSelector } from 'react-redux'
 import { selectFoundChild } from '../features/foundchild/FoundChildSlice'
 import { width } from '@mui/system'
+import conferences from '../assets/images/pngegg (3) 1.png'
+import announcement from '../assets/images/pngegg (1) 1.png'
+import compimg from '../assets/images/pngegg 1.png'
+import media from '../assets/images/pngegg (2) 1.png'
+import map from '../assets/images/map.png'
 
 const HeroSection = () => {
   const foundChildData = useSelector(selectFoundChild)
@@ -26,6 +31,8 @@ const HeroSection = () => {
   let size = hotspot.size
   let i = 0
   return (
+    <Box>
+    
     <Box sx={{ width: '100%', height: '800px', display: 'flex' }}>
       <Box sx={{ width: '100%', height: '800px', position: 'absolute' }}>
         <Box
@@ -94,6 +101,235 @@ const HeroSection = () => {
           marginLeft: '200px',
         }}
       ></Container>
+    </Box>
+    <Box
+    sx={{
+      position: 'relative',
+      width: '100%',
+      height: '215px',
+      display: 'flex',
+      marginTop: '20px',
+      marginBottom: '20px'
+      
+    }}
+    >
+      <Box sx={{position: 'relative', width: '8%', height: '300px',  background: '#FF5C58',}}/>
+      <Button sx={{
+        position: 'relative',
+        width: '20%',
+        height: '300px',
+        marginLeft: '10px',}}>
+      <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        background: 'rgba(254, 143, 143, 0.7)',       
+      }}>
+         <Box
+          component="img"
+          sx={{
+            width: '50%',
+            height: '50%',
+            position: 'relative',
+            display: 'flex',
+            marginLeft: '25%',
+            marginTop: '12%'
+            
+          }}
+          src={compimg}
+        >
+          
+          </Box> 
+            <Typography
+            variant="h4"
+            sx={{ position:'relative', height: '20%', width:'20%', display: 'flex', marginLeft:'20%', marginTop:'15px', color:'black'}}
+            >File a Complaint</Typography> 
+      </Box></Button>
+
+
+      <Button sx={{
+        position: 'relative',
+        width: '20%',
+        height: '300px',
+        marginLeft: '10px',}}>
+      <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        background: 'rgba(254, 143, 143, 0.7)',       
+      }}>
+         <Box
+          component="img"
+          sx={{
+            width: '50%',
+            height: '50%',
+            position: 'relative',
+            display: 'flex',
+            marginLeft: '25%',
+            marginTop: '10%',
+            
+          }}
+          src={announcement}
+        >
+          
+          </Box> 
+            <Typography
+            variant="h4"
+            sx={{ position:'relative', height: '20%', width:'20%', display: 'flex', marginLeft:'10%', color:'black'}}
+            >Important Announcements</Typography> 
+      </Box></Button>
+
+
+      <Button sx={{
+        position: 'relative',
+        width: '20%',
+        height: '300px',
+        marginLeft: '10px',}}>
+      <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        background: 'rgba(254, 143, 143, 0.7)',       
+      }}>
+         <Box
+          component="img"
+          sx={{
+            width: '50%',
+            height: '50%',
+            position: 'relative',
+            display: 'flex',
+            marginLeft: '25%',
+            marginTop: '12%',
+            
+          }}
+          src={media}
+        >
+          
+          </Box> 
+            <Typography
+            variant="h4"
+            sx={{ position:'relative', height: '20%', width:'20%', display: 'flex', marginLeft:'35%', marginTop:'15px', color:'black'}}
+            >Media</Typography> 
+      </Box></Button>
+      <Button sx={{
+        position: 'relative',
+        width: '20%',
+        height: '300px',
+        marginLeft: '10px',}}>
+      <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        background: 'rgba(254, 143, 143, 0.7)',       
+      }}>
+         <Box
+          component="img"
+          sx={{
+            width: '50%',
+            height: '50%',
+            position: 'relative',
+            display: 'flex',
+            marginLeft: '25%',
+            marginTop: '12%'
+            
+          }}
+          src={conferences}
+        >
+          
+          </Box> 
+            <Typography
+            variant="h4"
+            sx={{ position:'relative', height: '20%', width:'20%', display: 'flex', marginLeft:'20%', marginTop:'15px', color:'black'}}
+            >Conferences</Typography> 
+      </Box></Button>
+      
+      <Box sx={{position: 'relative', width: '10%', height: '300px', marginLeft: '15px', background: '#FF5C58',}}/>
+
+    </Box>
+    
+    <Box
+    sx={{
+      width: '100%',
+      height: '800px',
+      backgroundColor: 'rgba(254, 143, 143, 0.3)',
+      position: 'relative',
+      marginTop:'100px',
+      display: 'flex',
+    }}>
+      <Box
+      sx={{
+        height: '100%',
+        width: '50%',
+        position: 'absolute',
+        alignItems: 'left',
+        marginTop: '30px',
+        marginLeft: '30px',
+        }}>
+          <Typography
+          variant="h1"
+          sx={{ height: '20%', display: 'flex',  color: '#FF5C58'}}>
+            STATISTICS  </Typography>
+
+          <Typography
+          variant="h1"
+          sx={{ display: 'flex',  color: '#FF5C58',}}>        
+            2500
+          </Typography>
+          <Typography
+          variant="h4"
+          sx={{ height: '50px',display: 'flex',  color: '#FF5C58',}}>        
+            Reported Cases
+          </Typography>
+          <Typography
+          variant="h1"
+          sx={{  display: 'flex',  color: '#FF5C58',}}>        
+            2500
+          </Typography>
+          <Typography
+          variant="h4"
+          sx={{ height: '50px', display: 'flex',  color: '#FF5C58',}}>        
+            Nodal Officers Appointed
+          </Typography>
+          <Typography
+          variant="h1"
+          sx={{  display: 'flex',  color: '#FF5C58',}}>        
+            2500
+          </Typography>
+          <Typography
+          variant="h4"
+          sx={{  display: 'flex',  color: '#FF5C58',}}>        
+            Children Enrolled in School
+          </Typography>
+
+      </Box>
+      <Box
+       sx={{
+        height: '100%',
+        width: '50%',
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft:'50%'
+      }}>
+          <Box
+          component="img"
+          sx={{height:'90%'}}
+          
+          src={map}>
+            
+
+          </Box>
+        
+          
+
+      </Box>
+
+    </Box>
     </Box>
   )
 }
