e93996176cfd847910155852171119ba1a5deaba
Abhiram B.S.N <abhirambsn@gmail.com>
2022-08-26 14:22:03 +0530

    Skeleton Loading

--- frontend/src/features/nodal/NodalDashboard.js
+++ frontend/src/features/nodal/NodalDashboard.js
@@ -1,54 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import {
   selectFoundChild,
   getFoundChildStatus,
-} from '../foundchild/FoundChildSlice'
-import { getNodal, getNodalStatus } from './NodalSlice'
-import { useNavigate } from 'react-router-dom'
+} from "../foundchild/FoundChildSlice";
+import { getNodal, getNodalStatus } from "./NodalSlice";
+import { useNavigate } from "react-router-dom";
 
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
-import { Button } from '@mui/material'
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button, Skeleton } from "@mui/material";
 
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import SideBar from '../../components/Sidebar'
-import CancelIcon from '@mui/icons-material/Cancel'
-import CheckCircleIcon from '@mui/icons-material/CheckCircle'
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
-import sidebarMenus from '../../components/sidebarMenus'
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import SideBar from "../../components/Sidebar";
+import CancelIcon from "@mui/icons-material/Cancel";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import sidebarMenus from "../../components/sidebarMenus";
 
 function refresh() {
   if (!window.location.hash) {
-    window.location = window.location + '#loaded'
-    window.location.reload()
+    window.location = window.location + "#loaded";
+    window.location.reload();
   }
 }
 
 const NodalDashboard = () => {
-  refresh()
+  refresh();
   const navigate = useNavigate();
-  const statusFoundChild = useSelector(getFoundChildStatus)
-  const foundChildData = useSelector(selectFoundChild)
-  const statusNodal = useSelector(getNodalStatus)
-  const nodalData = useSelector((state) => getNodal(state))
-  const [childData, setChildData] = useState([])
+  const statusFoundChild = useSelector(getFoundChildStatus);
+  const foundChildData = useSelector(selectFoundChild);
+  const statusNodal = useSelector(getNodalStatus);
+  const nodalData = useSelector((state) => getNodal(state));
+  const [childData, setChildData] = useState([]);
+  const loading = useSelector(getNodalStatus);
+  const [uiLoading, setUiLoading] = useState(true);
 
   useEffect(() => {
-    if (statusFoundChild === 'Succeeded' && statusNodal === 'Succeeded') {
+    if (statusFoundChild === "Succeeded" && statusNodal === "Succeeded") {
       setChildData(
         foundChildData.filter(
           (child) =>
             child.state.toLowerCase() === nodalData.state.toLowerCase() &&
-            child.district.toLowerCase() === nodalData.district.toLowerCase(),
-        ),
-      )
+            child.district.toLowerCase() === nodalData.district.toLowerCase()
+        )
+      );
     }
   }, [
     statusFoundChild,
@@ -56,12 +58,12 @@ const NodalDashboard = () => {
     nodalData?.state,
     nodalData?.district,
     foundChildData,
-  ])
+  ]);
   return (
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
+        display: "flex",
+        justifyContent: "space-between",
       }}
     >
       <SideBar
@@ -70,186 +72,192 @@ const NodalDashboard = () => {
         header={sidebarMenus.nodal.header}
       />
 
-      {/* Table for XL Screens to L Screens */}
-      <TableContainer
-        sx={{
-          display: { xs: 'none', lg: 'inherit' },
-          mx: '20px',
-          mt: '100px',
-          maxHeight: '500px',
-        }}
-      >
-        <Table stickyHeader component={Paper}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Address</TableCell>
-              <TableCell>Is Accepted</TableCell>
-              <TableCell>Is Verified</TableCell>
-              <TableCell>Reported By</TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {childData.map((child) => (
-              <TableRow>
-                <TableCell>{child?.name}</TableCell>
-                <TableCell>{child?.address}</TableCell>
-                <TableCell>
-                  {child?.isAccepted ? (
-                    <CheckCircleIcon sx={{ color: 'green' }} />
-                  ) : (
-                    <CancelIcon sx={{ color: 'red' }} />
-                  )}
-                </TableCell>
-                <TableCell>
-                  {child?.isVerified ? (
-                    <CheckCircleIcon sx={{ color: 'green' }} />
-                  ) : (
-                    <CancelIcon sx={{ color: 'red' }} />
-                  )}
-                </TableCell>
-                <TableCell>{child?.reportedBy?.name}</TableCell>
-                <TableCell>
-                  <Button
-                    size="medium"
-                    variant="contained"
-                    onClick={() => navigate(`/child/${child?._id}`)}
-                  >
-                    <ArrowRightAltIcon />
-                    <Typography
-                      component="span"
-                      sx={{ display: { xs: 'none', md: 'block' } }}
-                    >
-                      View Details
-                    </Typography>
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        {/* , */}
-      </TableContainer>
+      {loading !== "Succeeded" ? (
+        <Skeleton variant="rectangular" width={210} height={118} />
+      ) : (
+        <>
+          {/* Table for XL Screens to L Screens */}
+          <TableContainer
+            sx={{
+              display: { xs: "none", lg: "inherit" },
+              mx: "20px",
+              mt: "100px",
+              maxHeight: "500px",
+            }}
+          >
+            <Table stickyHeader component={Paper}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Address</TableCell>
+                  <TableCell>Is Accepted</TableCell>
+                  <TableCell>Is Verified</TableCell>
+                  <TableCell>Reported By</TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {childData.map((child) => (
+                  <TableRow>
+                    <TableCell>{child?.name}</TableCell>
+                    <TableCell>{child?.address}</TableCell>
+                    <TableCell>
+                      {child?.isAccepted ? (
+                        <CheckCircleIcon sx={{ color: "green" }} />
+                      ) : (
+                        <CancelIcon sx={{ color: "red" }} />
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {child?.isVerified ? (
+                        <CheckCircleIcon sx={{ color: "green" }} />
+                      ) : (
+                        <CancelIcon sx={{ color: "red" }} />
+                      )}
+                    </TableCell>
+                    <TableCell>{child?.reportedBy?.name}</TableCell>
+                    <TableCell>
+                      <Button
+                        size="medium"
+                        variant="contained"
+                        onClick={() => navigate(`/child/${child?._id}`)}
+                      >
+                        <ArrowRightAltIcon />
+                        <Typography
+                          component="span"
+                          sx={{ display: { xs: "none", md: "block" } }}
+                        >
+                          View Details
+                        </Typography>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            {/* , */}
+          </TableContainer>
 
-      {/* Table for XS Screens to M Screens */}
-      <TableContainer
-        sx={{
-          display: { xs: 'none', md: 'inherit', lg: 'none' },
-          mx: '20px',
-          mt: '100px',
-          maxHeight: '500px',
-        }}
-      >
-        <Table stickyHeader component={Paper}>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <Typography varient="h4"> Name</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography varient="h4"> Reported By</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography varient="h4"> Is Verified</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography varient="h4"> Actions</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {childData.map((child) => (
-              <TableRow>
-                <TableCell>
-                  <Typography varient="h4"> {child?.name}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography varient="h4">
-                    {' '}
-                    {child?.reportedBy?.name}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  {child?.isVerified ? (
-                    <CheckCircleIcon sx={{ color: 'green' }} />
-                  ) : (
-                    <CancelIcon sx={{ color: 'red' }} />
-                  )}
-                </TableCell>
-                <TableCell>
-                  <Button
-                    size="small"
-                    variant="contained"
-                    onClick={() => navigate(`/child/${child?._id}`)}
-                  >
-                    <ArrowRightAltIcon />
-                    <Typography
-                      component="span"
-                      sx={{ display: { xs: 'none', md: 'block' } }}
-                    >
-                      View Details
-                    </Typography>
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          {/* Table for XS Screens to M Screens */}
+          <TableContainer
+            sx={{
+              display: { xs: "none", md: "inherit", lg: "none" },
+              mx: "20px",
+              mt: "100px",
+              maxHeight: "500px",
+            }}
+          >
+            <Table stickyHeader component={Paper}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <Typography varient="h4"> Name</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography varient="h4"> Reported By</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography varient="h4"> Is Verified</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography varient="h4"> Actions</Typography>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {childData.map((child) => (
+                  <TableRow>
+                    <TableCell>
+                      <Typography varient="h4"> {child?.name}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography varient="h4">
+                        {" "}
+                        {child?.reportedBy?.name}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      {child?.isVerified ? (
+                        <CheckCircleIcon sx={{ color: "green" }} />
+                      ) : (
+                        <CancelIcon sx={{ color: "red" }} />
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        size="small"
+                        variant="contained"
+                        onClick={() => navigate(`/child/${child?._id}`)}
+                      >
+                        <ArrowRightAltIcon />
+                        <Typography
+                          component="span"
+                          sx={{ display: { xs: "none", md: "block" } }}
+                        >
+                          View Details
+                        </Typography>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
 
-      {/* Table for M Screens to L Screens */}
-      <TableContainer
-        sx={{
-          display: { xs: 'inherit', md: 'none', lg: 'none' },
-          mx: '20px',
-          mt: '100px',
-          maxHeight: '500px',
-        }}
-      >
-        <Table stickyHeader component={Paper}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Reported By</TableCell>
-              <TableCell>Is Verified</TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {childData.map((child) => (
-              <TableRow>
-                <TableCell>{child?.name}</TableCell>
-                <TableCell>{child?.reportedBy?.name} </TableCell>
-                <TableCell>
-                  {child?.isVerified ? (
-                    <CheckCircleIcon sx={{ color: 'green' }} />
-                  ) : (
-                    <CancelIcon sx={{ color: 'red' }} />
-                  )}
-                </TableCell>
-                <TableCell>
-                  <Button
-                    size="small"
-                    variant="contained"
-                    onClick={() => navigate(`/child/${child?._id}`)}
-                  >
-                    <ArrowRightAltIcon />
-                    <Typography
-                      component="span"
-                      sx={{ display: { xs: 'none', md: 'block' } }}
-                    >
-                      View Details
-                    </Typography>
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          {/* Table for M Screens to L Screens */}
+          <TableContainer
+            sx={{
+              display: { xs: "inherit", md: "none", lg: "none" },
+              mx: "20px",
+              mt: "100px",
+              maxHeight: "500px",
+            }}
+          >
+            <Table stickyHeader component={Paper}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Reported By</TableCell>
+                  <TableCell>Is Verified</TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {childData.map((child) => (
+                  <TableRow>
+                    <TableCell>{child?.name}</TableCell>
+                    <TableCell>{child?.reportedBy?.name} </TableCell>
+                    <TableCell>
+                      {child?.isVerified ? (
+                        <CheckCircleIcon sx={{ color: "green" }} />
+                      ) : (
+                        <CancelIcon sx={{ color: "red" }} />
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        size="small"
+                        variant="contained"
+                        onClick={() => navigate(`/child/${child?._id}`)}
+                      >
+                        <ArrowRightAltIcon />
+                        <Typography
+                          component="span"
+                          sx={{ display: { xs: "none", md: "block" } }}
+                        >
+                          View Details
+                        </Typography>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
     </Box>
-  )
-}
+  );
+};
 
-export default NodalDashboard
+export default NodalDashboard;
--- frontend/src/features/users/UserDashboard.js
+++ frontend/src/features/users/UserDashboard.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getUser } from "./usersSlice";
+import { getUser, getUsersStatus } from "./usersSlice";
 import Box from "@mui/material/Box";
 import Sidebar from "../../components/Sidebar";
-import { Card, CardContent, Container, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Container,
+  Typography,
+  Skeleton,
+} from "@mui/material";
 
 import "react-step-progress-bar/styles.css";
-import {
-  // selectFoundChild,
-  getFoundChildByUser,
-} from "../foundchild/FoundChildSlice";
+import { getFoundChildByUser } from "../foundchild/FoundChildSlice";
 
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LogoutIcon from "@mui/icons-material/Logout";
@@ -36,6 +39,7 @@ const UserDashboard = () => {
   const childByUser = useSelector((state) =>
     getFoundChildByUser(state, user?._id)
   );
+  const loading = useSelector(getUsersStatus);
 
   let mail = user.email;
   let reportCount = 0;
@@ -55,16 +59,13 @@ const UserDashboard = () => {
       complaintResolved++;
     }
 
-    if (
-      mail === child?.reportedBy?.email &&
-      child?.isVerified 
-    ) {
+    if (mail === child?.reportedBy?.email && child?.isVerified) {
       complaintVerified++;
     }
   });
 
   // allChild.forEach((child) => {
-    
+
   // });
 
   const navigate = useNavigate();
@@ -85,120 +86,127 @@ const UserDashboard = () => {
         sectionList={sidebarMenus.user.sectionList}
         header={sidebarMenus.user.header}
       />
-      <Container sx={{ paddingTop: "20px" }}>
-        <Card sx={{ minWidth: 275, marginTop: "60px" }}>
-          <CardContent
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Typography sx={{ fontSize: 20, textAlign: "center" }} gutterBottom>
-              Welcome, <strong>{user?.name}</strong>
-            </Typography>
-            <Box
+      {loading !== "Succeeded" ? (
+        <Skeleton variant="rectangular" width={210} height={118} />
+      ) : (
+        <Container sx={{ paddingTop: "20px" }}>
+          <Card sx={{ minWidth: 275, marginTop: "60px" }}>
+            <CardContent
               sx={{
-                borderRadius: "100px",
-                backgroundColor: "#F3F3F3",
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "center",
-                padding: "10px",
-                width: { lg: "200px" },
               }}
             >
-              <AccountCircleIcon
+              <Typography
+                sx={{ fontSize: 20, textAlign: "center" }}
+                gutterBottom
+              >
+                Welcome, <strong>{user?.name}</strong>
+              </Typography>
+              <Box
                 sx={{
-                  color: "primary",
-                  display: { xs: "none", lg: "inline" },
+                  borderRadius: "100px",
+                  backgroundColor: "#F3F3F3",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  padding: "10px",
+                  width: { lg: "200px" },
                 }}
-                fontSize="large"
-              />
-              <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <Typography
+              >
+                <AccountCircleIcon
                   sx={{
-                    fontSize: 14,
+                    color: "primary",
                     display: { xs: "none", lg: "inline" },
-                    color: "gray",
                   }}
-                >
-                  Account Type
-                </Typography>
-                <Typography
-                  sx={{ fontSize: 14, display: { xs: "none", lg: "inline" } }}
-                >
-                  {uTypes[user?.acType]}
-                </Typography>
+                  fontSize="large"
+                />
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <Typography
+                    sx={{
+                      fontSize: 14,
+                      display: { xs: "none", lg: "inline" },
+                      color: "gray",
+                    }}
+                  >
+                    Account Type
+                  </Typography>
+                  <Typography
+                    sx={{ fontSize: 14, display: { xs: "none", lg: "inline" } }}
+                  >
+                    {uTypes[user?.acType]}
+                  </Typography>
+                </Box>
+                <LogoutIcon
+                  sx={{ color: "red", cursor: "pointer" }}
+                  fontSize="medium"
+                  onClick={() => navigate("/logout")}
+                />
               </Box>
-              <LogoutIcon
-                sx={{ color: "red", cursor: "pointer" }}
-                fontSize="medium"
-                onClick={() => navigate("/logout")}
-              />
-            </Box>
-          </CardContent>
-        </Card>
-        <Box
-          sx={{
-            paddingTop: "20px",
-            display: "flex",
-            flexDirection: { xs: "column", lg: "row" },
-            justifyContent: "space-between",
-          }}
-        >
-          <Card
-            sx={{
-              backgroundColor: "primary.main",
-              color: "primary.contrastText",
-              marginBottom: "10px",
-            }}
-          >
-            <CardContent>
-              <Typography sx={{ fontSize: 24 }} gutterBottom>
-                Children Reported
-              </Typography>
-              <Typography sx={{ fontSize: 20 }} gutterBottom>
-                <strong>{reportCount}</strong>
-              </Typography>
             </CardContent>
           </Card>
-          <Card
+          <Box
             sx={{
-              backgroundColor: "primary.main",
-              color: "primary.contrastText",
-              marginBottom: "10px",
+              paddingTop: "20px",
+              display: "flex",
+              flexDirection: { xs: "column", lg: "row" },
+              justifyContent: "space-between",
             }}
           >
-            <CardContent>
-              <Typography sx={{ fontSize: 24 }} gutterBottom>
-                Complaints Verified
-              </Typography>
-              <Typography sx={{ fontSize: 20 }} gutterBottom>
-                <strong>{complaintVerified}</strong>
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card
-            sx={{
-              backgroundColor: "primary.main",
-              color: "primary.contrastText",
-            }}
-          >
-            <CardContent>
-              <Typography sx={{ fontSize: 24 }} gutterBottom>
-                Complaints Resolved
-              </Typography>
-              <Typography sx={{ fontSize: 20 }} gutterBottom>
-                <strong>{complaintResolved}</strong>
-              </Typography>
-            </CardContent>
-          </Card>
-        </Box>
-        <Box display="flex" sx={{ paddingTop: "20px" }}>
-          <ComplaintStatus heading={true} />
-        </Box>
-      </Container>
+            <Card
+              sx={{
+                backgroundColor: "primary.main",
+                color: "primary.contrastText",
+                marginBottom: "10px",
+              }}
+            >
+              <CardContent>
+                <Typography sx={{ fontSize: 24 }} gutterBottom>
+                  Children Reported
+                </Typography>
+                <Typography sx={{ fontSize: 20 }} gutterBottom>
+                  <strong>{reportCount}</strong>
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card
+              sx={{
+                backgroundColor: "primary.main",
+                color: "primary.contrastText",
+                marginBottom: "10px",
+              }}
+            >
+              <CardContent>
+                <Typography sx={{ fontSize: 24 }} gutterBottom>
+                  Complaints Verified
+                </Typography>
+                <Typography sx={{ fontSize: 20 }} gutterBottom>
+                  <strong>{complaintVerified}</strong>
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card
+              sx={{
+                backgroundColor: "primary.main",
+                color: "primary.contrastText",
+              }}
+            >
+              <CardContent>
+                <Typography sx={{ fontSize: 24 }} gutterBottom>
+                  Complaints Resolved
+                </Typography>
+                <Typography sx={{ fontSize: 20 }} gutterBottom>
+                  <strong>{complaintResolved}</strong>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+          <Box display="flex" sx={{ paddingTop: "20px" }}>
+            <ComplaintStatus heading={true} />
+          </Box>
+        </Container>
+      )}
     </Box>
   );
 };
