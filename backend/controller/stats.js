04df5f117fdda1a53a18267d18301a2148252b0a
Abhiram B.S.N <abhirambsn@gmail.com>
2022-08-26 09:04:30 +0530

    Homepage map fixed

--- backend/controller/stats.js
+++ backend/controller/stats.js
@@ -1,8 +1,31 @@
 const nodalOfficer = require("../model/nodalOfficer");
+const foundChild = require("../model/foundChild");
 
 const getNodalCount = async (req, res) => {
-  const allNodal = await nodalOfficer.find({ state: req?.body?.state }).exec();
-  return res.status(200).send({ count: allNodal.length });
+  let allNodal = null;
+  let allChildrenReported = null;
+  let allChildrenResolved = null;
+  if (!req?.body?.state) {
+    allNodal = await nodalOfficer.find().exec();
+    allChildrenReported = await foundChild.find().exec();
+    allChildrenResolved = await foundChild
+      .find({ isVerified: true, inSchool: true })
+      .exec();
+  } else {
+    allNodal = await nodalOfficer.find({ state: req?.body?.state }).exec();
+    allChildrenReported = await foundChild
+      .find({ state: req?.body?.state })
+      .exec();
+    allChildrenResolved = await foundChild
+      .find({ state: req?.body?.state, isVerified: true, inSchool: true })
+      .exec();
+  }
+
+  return res.status(200).send({
+    nodalCount: allNodal.length,
+    childrenReported: allChildrenReported.length,
+    childrenResolved: allChildrenResolved.length,
+  });
 };
 
 module.exports = { getNodalCount };
