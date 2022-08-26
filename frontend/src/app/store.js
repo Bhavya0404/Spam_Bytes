c2a2f8e51e49b279fd5f2b8240a213b4dc16ac66
Abhiram B.S.N <abhirambsn@gmail.com>
2022-08-26 06:29:00 +0530

    Hotspot Map Fixed

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import usersReducer from '../features/users/usersSlice'
-import ngoReducer from '../features/ngo/ngoSlice'
-import nodalReducer from '../features/nodal/NodalSlice'
-import FoundChildReducer from '../features/foundchild/FoundChildSlice'
-import TicketSlice from '../features/ticket/TicketSlice'
-
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "../features/users/usersSlice";
+import ngoReducer from "../features/ngo/ngoSlice";
+import nodalReducer from "../features/nodal/NodalSlice";
+import FoundChildReducer from "../features/foundchild/FoundChildSlice";
+import TicketSlice from "../features/ticket/TicketSlice";
+import Statistics from "../features/statistics/Statistics";
 
 export const store = configureStore({
   reducer: {
@@ -12,6 +12,7 @@ export const store = configureStore({
     ngo: ngoReducer,
     nodal: nodalReducer,
     foundchild: FoundChildReducer,
-    ticketslice: TicketSlice
+    ticketslice: TicketSlice,
+    statename: Statistics,
   },
-})
+});
